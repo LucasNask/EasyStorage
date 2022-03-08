ed27a61b5cff75fa6c0a40bb3dc7733e366197fb
LucasNask <lucas_nascimento1@hotmail.com>
2022-03-07 22:06:36 -0300

    Frist commit :)

---
+++ src/main/java/com/ni/easystorage/easystorage/EasystorageApplication.java
@@ -0,0 +1,13 @@
+package com.ni.easystorage.easystorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasystorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasystorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ni/easystorage/easystorage/EasystorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ni.easystorage.easystorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasystorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
