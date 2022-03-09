81b82315dc6d940274e4059c66159eb7163bb3c4
igoryy <igor.siviero1@gmail.com>
2022-03-08 21:24:34 -0300

    Implementar docker compose e config redis

--- src/main/java/com/ni/easystorage/easystorage/EasystorageApplication.java
+++ src/main/java/com/ni/easystorage/easystorage/EasystorageApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EasystorageApplication {
+public class 	EasystorageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasystorageApplication.class, args);
