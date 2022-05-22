663d5244e6900acd5d634bf1a2ace957e8132f12
igoryy <igor.siviero1@gmail.com>
2022-05-21 22:01:24 -0300

    fix ehcache

---
+++ src/main/java/com/ni/easystorage/easystorage/config/EventLogger.java
@@ -0,0 +1,19 @@
+package com.ni.easystorage.easystorage.config;
+
+import org.ehcache.event.CacheEvent;
+import org.ehcache.event.CacheEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class EventLogger implements CacheEventListener<Object, Object> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventLogger.class);
+
+    @Override
+    public void onEvent(CacheEvent<?, ?> event) {
+        LOGGER.info("Event: " + event.getType() + " Key: " + event.getKey() + " old value: " + event.getOldValue() + " new value: " + event.getNewValue());
+    }
+}
